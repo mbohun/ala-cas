12a7c9fa4a9648218b273a2c8db09d0360f6ec9e
peter.flemming@csiro.au <peter.flemming@csiro.au>
2010-08-30 07:27:25 +0000

    Stored procedure definition for Emmet db
    git-svn-id: http://ala-bie.googlecode.com/svn/trunk/ala-cas@1683 de2a7fd8-b20c-11de-9ad3-fd5786690477

---
+++ sp_get_user_attributes.sql
@@ -0,0 +1,10 @@
+delimiter //
+create procedure sp_get_user_attributes (p_username varchar(30))
+begin
+select 'email' as 'key', email as 'value' from users where username=p_username
+union select 'firstname' as 'key', firstname as 'value' from users where username=p_username
+union select 'lastname' as 'key', lastname as 'value' from users where username=p_username
+union select 'authority' as 'key', group_concat(a.authority) as 'value' from authorities a, users u where a.userid=u.userid and u.username=p_username;
+end
+//
+delimiter ;
