16aa6bc19d3c3293f5b261f44b42ef5426a8f1bb
peter.flemming@csiro.au <peter.flemming@csiro.au>
2010-10-01 11:59:21 +0000

    Added userid to list of retrieved user attributes
    git-svn-id: http://ala-bie.googlecode.com/svn/trunk/ala-cas@1926 de2a7fd8-b20c-11de-9ad3-fd5786690477

--- sp_get_user_attributes.sql
+++ sp_get_user_attributes.sql
@@ -6,6 +6,7 @@ begin
 select 'email' as 'key', email as 'value' from users where username=p_username
 union select 'firstname' as 'key', firstname as 'value' from users where username=p_username
 union select 'lastname' as 'key', lastname as 'value' from users where username=p_username
+union select 'userid' as 'key', cast(userid as char) as 'value' from users where username=p_username
 union select 'authority' as 'key', group_concat(a.authority) as 'value' from authorities a, users u where a.userid=u.userid and u.username=p_username;
 end
 //
