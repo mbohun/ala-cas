5c80f4eeb12aa6431fb309ebb27d4af83a65ffdc
djtfmartin <djtfmartin@gmail.com>
2013-09-26 04:26:41 +0000

    updated to use the user_role table instead of authorities
    git-svn-id: http://ala-bie.googlecode.com/svn/trunk/ala-cas@4247 de2a7fd8-b20c-11de-9ad3-fd5786690477

--- sp_get_user_attributes.sql
+++ sp_get_user_attributes.sql
@@ -1,14 +1,23 @@
 DROP PROCEDURE IF EXISTS sp_get_user_attributes;
-
 delimiter //
-create procedure sp_get_user_attributes (p_username varchar(255))
+create procedure sp_get_user_attributesXXXX (p_username varchar(255))
 begin
 select 'email' as 'key', email as 'value' from users where username=p_username
 union select 'firstname' as 'key', firstname as 'value' from users where username=p_username
 union select 'lastname' as 'key', lastname as 'value' from users where username=p_username
 union select 'displayName' as 'key', profiles.value from profiles inner join users ON users.userid=profiles.userid where users.username=p_username and profiles.property='displayName'
 union select 'userid' as 'key', cast(userid as char) as 'value' from users where username=p_username
-union select 'authority' as 'key', group_concat(a.authority) as 'value' from authorities a, users u where a.userid=u.userid and u.username=p_username;
+union select 'role' as 'key', group_concat(a.role_id) as 'value' from user_role a, users u where a.user_id=u.userid and u.username=p_username;
+end
+//
+delimiter ;
+
+
+DROP PROCEDURE IF EXISTS sp_get_user_authorities;
+delimiter //
+create procedure sp_get_user_authorities()
+begin
+select username, group_concat(ur.role_id) as 'authorities' from users u, user_role ur where u.userid=ur.user_id group by username;
 end
 //
 delimiter ;
